fa87e2b6575fb408a5f3d32d4dcbde15ca587cd7
sakibahmad <sakibsiddiqui21@gmail.com>
2017-03-08 20:19:40 +0530

    assignment7.2
    String

---
+++ src/Findsub.java
@@ -0,0 +1,33 @@
+
+/*
+ * Program to find a substring in a String by using an inbuilt method of String class.
+ */
+//import Scanner for user input
+import java.util.Scanner;
+
+//define a class Findsub
+public class Findsub {
+	
+	public static void main(String[] args) {
+		// allowing user input
+		
+		Scanner o = new Scanner(System.in);
+		System.out.println("Enter the string");
+		// enter substring to find
+		
+		String s = o.nextLine();
+		System.out.println("Enter the sub string you want to search");
+		String s1 = o.nextLine();
+		
+		// condition to check whether sub string is present
+		// using contains method
+		
+		if (s.contains(s1)) {
+			System.out.println("sub string found");
+		} else {
+
+			System.out.println("sub string not found");
+		}
+	}
+
+}
